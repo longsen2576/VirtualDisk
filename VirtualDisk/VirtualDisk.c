befb20f67a0a4c36fc401fd5b0dde3a0b73f767e
longsen <jinlong2204@126.com>
2017-05-06 17:14:38 +0800

    vitual disk first submit

---
+++ VirtualDisk/VirtualDisk.c
@@ -0,0 +1,30 @@
+/******************************************
+*Author:longsen
+*Date:2017-5-5
+*File:VirtualDisk.c
+*******************************************/
+
+#include <ntddk.h>
+
+#include "VitualDisk.h"
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT driver_obj, PUNICODE_STRING reg_path)
+{
+	UNREFERENCED_PARAMETER(driver_obj);
+	UNREFERENCED_PARAMETER(reg_path);
+	//force to insert a break point 
+	DbgBreakPoint();
+
+	NTSTATUS status = STATUS_SUCCESS;
+	DbgPrint("Driver load");
+	DbgPrint("just for test driver install ");
+
+	return status;
+}
+
+
+VOID DriverUnload(PDRIVER_OBJECT driver_obj)
+{
+	UNREFERENCED_PARAMETER(driver_obj);
+	DbgPrint("Driver unload");
+}
---
+++ VirtualDisk/VitualDisk.h
@@ -0,0 +1,14 @@
+/******************************************
+*Author:longsen
+*Date:2017-5-5
+*File:VirtualDisk.h
+*******************************************/
+
+#pragma once
+
+#include <ntddk.h>
+
+VOID DriverUnload(PDRIVER_OBJECT driver_obj);
+
+NTSTATUS DriverEntry(PDRIVER_OBJECT driver_obj, PUNICODE_STRING reg_path);
+
